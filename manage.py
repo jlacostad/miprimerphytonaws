1165ff00d8b739f7f3fd5775a4679d478991fe9d
Javier Acosta <javieraacostaleon@gmail.com>
2020-09-15 22:20:23 -0500

    Fix: texto

--- manage.py
+++ manage.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
+"""Django's command-line utility for administrative tasks.HOLa cuñiiiissss """
 import os
 import sys
 
